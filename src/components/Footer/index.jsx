09970f15bf34231273309fa0fc8a9cef31717d81
huy <huy@gmail.com>
2023-07-14 17:29:18 +0700

    huydev

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,25 @@
 const Footer = () => {
-    return ( 
-        <h1>Footer</h1>
-     );
-}
- 
+  return (
+    <div
+      className="container-fluid"
+      style={{ position: "fixed", bottom: 0, left: 0, right: 0, width: "100%"  }}
+    >
+      <div
+        className="row border-top d-flex justify-content-between text-center"
+        style={{ padding: "1rem" }}
+      >
+        <div className="col">
+          <span>3 Task</span>
+        </div>
+        <div className="col">
+          <span>0 Complete</span>
+        </div>
+        <div className="col">
+          <span>3 Open</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,31 @@
 const Header = () => {
-    return ( 
-        <h1>Banner</h1>
-     );
-}
+  return (
+    <div className="container-fluid">
+      <div
+        className="row border-bottom"
+        style={{ padding: "1rem", overflowX: "hidden" }}
+      >
+        <div className="col-2 d-flex d-sm-none align-items-center justify-content-center">
+          <span>
+            <i className="fa-solid fa-align-justify" />
+          </span>
+        </div>
+        <div className="col-8 col-sm-6 d-flex align-items-center justify-content-center justify-content-sm-start">
+          <img
+            src="https://i.imgur.com/bbnrc1T.png"
+            alt="Logo"
+            height={32}
+            width={34}
+            className="ms-3 me-3"
+          />
+        </div>
+        <div className="col-8 col-sm-6 d-none d-sm-flex align-items-start justify-content-end">
+          <span>Logout</span>
+        </div>
  
+      </div>
+    </div>
+  );
+};
+
 export default Header;
